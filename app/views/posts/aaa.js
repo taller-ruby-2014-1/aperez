a34be4eedd69054e2a8c2f7706c5c5dfd9de6fdc
Alumno <alumno@localhost.localdomain>
2014-05-28 16:06:56 -0400

    hasta la 6 -.-

---
+++ app/views/posts/aaa.js
@@ -0,0 +1,11 @@
+var contador = null;
+var step = 1000;
+
+var setop +=500;
+//Iniciar Contador
+contador = setInterval(function(){
+	//actualizar contador...
+}, step);
+
+//Detiene
+clearInteval(contador);
